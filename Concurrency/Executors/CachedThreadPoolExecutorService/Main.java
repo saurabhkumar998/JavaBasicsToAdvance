546f778009595321a1799efa427a389955f2d897
saurabh <saurabh.kumar.2382000@gmail.com>
2024-08-24 02:32:03 +0530

    CachedThreadPoolExecutorService

--- Concurrency/Executors/CachedThreadPoolExecutorService/Main.java
+++ Concurrency/Executors/CachedThreadPoolExecutorService/Main.java
@@ -2,11 +2,43 @@ package Concurrency.Executors.CachedThreadPoolExecutorService;
 
 import Concurrency.Executors.ThreadColor;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.sql.Time;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
+
 	public static void main(String[] args) {
+		var multiExecutor = Executors.newCachedThreadPool();
+		List<Callable<Integer>> taskList = List.of(
+				new Callable<Integer>() {
+					@Override
+					public Integer call() throws Exception {
+						return Main.sum(1, 10, 1, "red");
+					}
+				},
+				// the below code is shorter version of the above code - here we are using lambda expression instead of an anonymous class
+		//		() -> Main.sum(1, 10, 1, "red"),
+				() -> Main.sum(10, 100, 10, "blue"),
+				() -> Main.sum(2, 20, 2, "green")
+		);
+
+		try{
+			// the invokeAll() method takes a list of callable and returns a list of Future object
+			var results = multiExecutor.invokeAll(taskList);
+			for(var result : results) {
+				// there is a possibility that the task is still being executed hence we are giving a timeout of 500 ms, if the task completes within this time then we get the response or else we return to the main thread
+				System.out.println(result.get(500, TimeUnit.MILLISECONDS));
+			}
+		}
+		catch (InterruptedException | TimeoutException | ExecutionException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			multiExecutor.shutdown();
+		}
+	}
+	public static void cachedmain(String[] args) {
 		var multiExecutor = Executors.newCachedThreadPool();
 
 		try{
